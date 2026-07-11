a46a585cba55c567441dd956ff45db9f394ec25b
Dinh Dinh Khhoi <131263466+dinhkhoiIT15@users.noreply.github.com>
2026-07-12 01:29:24 +0700

    command build product and server

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,17 +3,30 @@ import taskRoute from './routes/tasksRoutes.js';
 import {connectDB} from './config/db.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import path from 'path';
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 const app = express();
 
 app.use(express.json()); //middleware để parse dữ liệu JSON từ request body
-app.use(cors({origin: 'http://localhost:5173'}));
+
+if(process.env.NODE_ENV !== 'production'){
+      app.use(cors({origin: 'http://localhost:5173'}));
+}
 
 app.use('/api/tasks', taskRoute);
+
+if(process.env.NODE_ENV === 'production'){
+      app.use(express.static(path.join(__dirname, '../frontend/dist')));
+
+      app.get("*", (req, res) => {
+            res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
+      });
+}
 
 connectDB().then(() => {
       app.listen(PORT, () => {
